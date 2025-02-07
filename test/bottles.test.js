84c60db448504dcb638d18bc447dc085eb9c2261
Nathan Douglas <github@darkdell.net>
2025-02-06 20:19:37 -0500

    Moving BottleVerse tests, adds tests for verse 7 and verse 6.

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -1,50 +1,6 @@
-import { Bottles } from '../lib/bottles';
+import { BottleNumber, BottleVerse, Bottles } from '../lib/bottles';
 
 describe('Bottles', () => {
-  test('the first verse', () => {
-    const expected =
-      '99 bottles of beer on the wall, ' +
-      '99 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '98 bottles of beer on the wall.\n';
-    expect(new Bottles().verse(99)).toBe(expected);
-  });
-
-  test('another verse', () => {
-    const expected =
-      '3 bottles of beer on the wall, ' +
-      '3 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '2 bottles of beer on the wall.\n';
-    expect(new Bottles().verse(3)).toBe(expected);
-  });
-
-  test('verse 2', () => {
-    const expected =
-      '2 bottles of beer on the wall, ' +
-      '2 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '1 bottle of beer on the wall.\n';
-    expect(new Bottles().verse(2)).toBe(expected);
-  });
-
-  test('verse 1', () => {
-    const expected =
-      '1 bottle of beer on the wall, ' +
-      '1 bottle of beer.\n' +
-      'Take it down and pass it around, ' +
-      'no more bottles of beer on the wall.\n';
-    expect(new Bottles().verse(1)).toBe(expected);
-  });
-
-  test('verse 0', () => {
-    const expected =
-      'No more bottles of beer on the wall, ' +
-      'no more bottles of beer.\n' +
-      'Go to the store and buy some more, ' +
-      '99 bottles of beer on the wall.\n';
-    expect(new Bottles().verse(0)).toBe(expected);
-  });
 
   test('a couple verses', () => {
     const expected =
@@ -383,4 +339,70 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
 `;
     expect(new Bottles().song()).toBe(expected);
   });
+});
+
+describe('BottleVerse', () => {
+  test('verse general rule upper bound', () => {
+    const expected =
+      '99 bottles of beer on the wall, ' +
+      '99 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of beer on the wall.\n';
+    expect(BottleVerse.lyrics(99)).toBe(expected);
+  });
+
+  test('verse general rule lower bound', () => {
+    const expected =
+      '3 bottles of beer on the wall, ' +
+      '3 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '2 bottles of beer on the wall.\n';
+    expect(BottleVerse.lyrics(3)).toBe(expected);
+  });
+
+  test('verse 7', () => {
+    const expected =
+      '7 bottles of beer on the wall, ' +
+      '7 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '1 six-pack of beer on the wall.\n';
+    expect(BottleVerse.lyrics(7)).toBe(expected);
+  });
+
+  test('verse 6', () => {
+    const expected =
+      '1 six-pack of beer on the wall, ' +
+      '1 six-pack of beer.\n' +
+      'Take one down and pass it around, ' +
+      '5 bottles of beer on the wall.\n';
+    expect(BottleVerse.lyrics(6)).toBe(expected);
+  });
+
+  test('verse 2', () => {
+    const expected =
+      '2 bottles of beer on the wall, ' +
+      '2 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '1 bottle of beer on the wall.\n';
+    expect(BottleVerse.lyrics(2)).toBe(expected);
+  });
+
+  test('verse 1', () => {
+    const expected =
+      '1 bottle of beer on the wall, ' +
+      '1 bottle of beer.\n' +
+      'Take it down and pass it around, ' +
+      'no more bottles of beer on the wall.\n';
+    expect(BottleVerse.lyrics(1)).toBe(expected);
+  });
+
+  test('verse 0', () => {
+    const expected =
+      'No more bottles of beer on the wall, ' +
+      'no more bottles of beer.\n' +
+      'Go to the store and buy some more, ' +
+      '99 bottles of beer on the wall.\n';
+    expect(BottleVerse.lyrics(0)).toBe(expected);
+  });
+
 });
